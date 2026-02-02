2ba2fdff4b81b5df4e5bee445b7068a33fa4b9e5
PashaKrasnoporov <61505136+PashaKrasnoporov@users.noreply.github.com>
2026-02-02 22:10:18 +0200

    Delete static directory

--- static/js/results.js
+++
@@ -1,7 +0,0 @@
-const result = localStorage.getItem('enneagram_result');
-if (!result) {
-    alert('Ви ще не пройшли тест. Результати доступні лише після проходження!');
-    window.location.href = '/test';
-}
-
-// Далі твій код для отримання та показу результату
